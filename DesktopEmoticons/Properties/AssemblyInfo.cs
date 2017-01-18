5c952423c670bb4e96eabd381ca8e089d3a8a58f
Ben H <ben@benmakesgames.com>
2017-01-17 19:15:46 -0500

    including executable, in case people just want to D/L it. also, put my name in the AssemblyInfo.cs. if it gets passed around as-is, I'd like there to be SOME evidence of its original creator.

--- DesktopEmoticons/Properties/AssemblyInfo.cs
+++ DesktopEmoticons/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DesktopEmoticons")]
+[assembly: AssemblyTitle("DesktopEmoticons, by Ben Hendel-Doying")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DesktopEmoticons")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("DesktopEmoticons, by Ben Hendel-Doying")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
